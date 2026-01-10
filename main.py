9a10d508d2142b747ecc832f333e886f7eec91cf
CeMoiCheri <markus_stich@gmx.de>
2026-01-10 13:30:12 +0100

    adapting for render

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from flask import Flask, jsonify, request
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
+@app.route("/run")
+def run():
+    # Replace this with your actual logic
+    result = sum(range(10))
+    return jsonify({"result": result})
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
